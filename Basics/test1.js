c2a7ac66a161eb02feeaae6596196c3d5cda2af1
Ashwini-Sutkar <101690896+Ashwini-Sutkar@users.noreply.github.com>
2023-12-19 19:16:27 +0000

    variable learned

--- Basics/test1.js
+++ Basics/test1.js
@@ -1,3 +1,18 @@
 
 // Terminal type :- "node Basics/test1.js " command 
-console.log("Ashwini");
+// console.log("Ashwini");
+
+// variables in javascript
+const accountId =112255    // Only write one time value cant use same variable name again;
+let accountEmail= "ashwini@gmail.com"   // Block scope variable
+var accountPassword =" 122345"   //  Its not preffer  coz isuue with block and functional scope.  
+accountCity= "Pune"
+//  accountId =2
+// console.log(accountId);
+
+accountEmail=" hk@gmail.com"
+accountPassword= "1234"
+accountCity= "gurgaon"
+let accountName;   // undefined 
+
+console.table([accountEmail, accountPassword, accountCity, accountId,accountName])
