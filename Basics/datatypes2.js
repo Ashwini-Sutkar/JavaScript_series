954997ddb776e3fa22c8b388e85b36c73e053321
Ashwini-Sutkar <101690896+Ashwini-Sutkar@users.noreply.github.com>
2023-12-19 19:40:49 +0000

    datatypes

---
+++ Basics/datatypes2.js
@@ -0,0 +1,23 @@
+"use strict"  //  treat all js code in newer version;
+
+// alert(3 + 3)   --WE ARE USING NODE JS NOT BROWSER.
+
+// CODE READABILITY SHOULD BE HIGH.
+
+// console.log("hitesh");
+
+// Datatypes:=
+// 1. Number => 12
+// 2. String => "shh"
+// 3. Boolean => True or false
+// 4. null => standalone Value   let state= null;
+// 5. undefined =>  let state; 
+// 6. symbol   => unique mostly in react
+// 7. object
+
+console.log(typeof null);  // nul is object
+console.log(typeof undefined);
+console.log(typeof number);
+
+
+
