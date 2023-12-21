e74776355484e62595d65dc1a692dfb30ddde250
Ashwini-Sutkar <101690896+Ashwini-Sutkar@users.noreply.github.com>
2023-12-21 10:53:03 +0000

    datatypes

---
+++ Basics/datatype4.js
@@ -0,0 +1,51 @@
+// HITESH CHAUDHARY GITHUB LINK
+//github.com/hiteshchoudhary/js-hindi-youtube/blob/main/01_basics/datatypes-summary.js
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
+// PRIMITIVE TYPES
+//=>7.   String, NUMBER,  BOOLEAN, NULL, UNDEFINED, Symbol, BigInt
+
+// (RESFERENCES) NON PRIMITIVE TYPE
+// =>3.  Array, Object, Function
+
+
+//  1.Number
+const score = 100;  
+const scoreValue = 100.3;
+// 2. String
+const name= "Ashwini"
+
+// 3.Boolean
+const IsLoggedIn =false;
+// 4.null
+const outsideTemp =null;
+//5. undefined
+ let userEmail;
+ //6. symbol
+ const id = Symbol('123')
+ const anotherId= Symbol('123')
+ console.log(id===anotherId);  // False
+
+//  const bigNumber = 12345678n
+//  console.log(bigNumber);
+
+
+
+// Non Primitive
+// ARRAY 
+const arr = ["Ash","Ashu","Ashwini","Hitesh"];
+
+// OBJECT
+const obj ={
+    Fname: "Ash",
+    Lname: "Sutkar"
+}
+
+// FUNCTION
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+
+
+
