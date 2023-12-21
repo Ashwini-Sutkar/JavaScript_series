7d834692951eb5f7bfba3bc2ebdb2532e4afce77
Ashwini-Sutkar <101690896+Ashwini-Sutkar@users.noreply.github.com>
2023-12-21 09:55:37 +0000

    oprator

---
+++ Basics/operator.js
@@ -0,0 +1,72 @@
+// let score =null;
+// // let score =undefined;  // NAN
+// console.log( typeof score);  
+//  => Number
+
+let scor = "";
+// console.log( typeof scor);
+// // => String
+
+let sc= "33acs";
+// console.log( typeof sc);
+// => String
+
+let num = Number(sc)
+// console.log(typeof num); // number
+// console.log(num);// NaN Not A Number
+
+
+// NOTES:-  TypeOf
+// 1.  "33" => 33   converted in Number line 14
+// 2. 33abc => NaN  but type is number
+// 3. True => 1 & False => 0
+
+// let isUserLogged=1;  => true
+// let isUserLogged=0;  =>false
+// let isUserLogged="";  => False
+//  let isUserLogged="hitesh"; => true
+
+
+// let IsLoggedIn= Boolean(isUserLogged);
+// console.log(IsLoggedIn);
+
+//  *****************************OPERATORS**************************************
+
+// let value =3
+// let negValue=-value
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+// let str1 = "Hello";
+// let str2= " Hitesh";
+// let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log(2 +2 );
+// console.log(2+"2");
+// console.log("2"+2);
+// console.log("2"+"2");
+// console.log("2"+2+2);   // When  
+// console.log(2+2+"2");
+
+
+// *********************COMPARISION************************
+
+console.log(null < 0);     // false
+console.log(null > 0);     // false
+console.log(null >= 0);    // true
+console.log( null <=0);    // true
+console.log( null === 0);  // false
+
+
+console.log(undefined < 0);      // false
+console.log(undefined > 0);      // false
+console.log(undefined >= 0);     // false
+console.log( undefined <=0);     // false
+console.log( undefined === 0);   // false
